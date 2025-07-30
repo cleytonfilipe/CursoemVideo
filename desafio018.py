fb10d655b6d48fbd319aa850ce6203d63937c867
cleytonfilipe <cleyton.sdv@gmail.com>
2025-07-30 00:22:06 -0300

    testes e exercícios de módulos

--- desafio018.py
+++ desafio018.py
@@ -4,5 +4,5 @@ a1 = input('Qual o nome do primeiro aluno? ')
 a2 = input('Qual o nome do segundo aluno? ')
 a3 = input('Qual o nome do terceiro aluno? ')
 a4 = input('Qual o nome do quarto aluno? ')
-esc = random.(a1, a2, a3, a4)
+esc = random
 print('O aluno escolhido foi {}!'.format(esc))
