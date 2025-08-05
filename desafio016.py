0c74ad4180cbb3b85e2d309aa8875474581a8bdb
cleytonfilipe <cleyton.sdv@gmail.com>
2025-08-04 23:46:40 -0300

    correção de exercícios

--- desafio016.py
+++ desafio016.py
@@ -1,4 +1,4 @@
-import math
+from math import trunc
 print('Desafio 016 - Python - Curso em Video')
 num = float(input('Digite um número: '))
-print('O número {} tem a parte inteira {}.'.format(num, math.trunc(num)))
+print('O número {} tem a parte inteira {}.'.format(num, trunc(num)))
--- desafio017.py
+++ desafio017.py
@@ -1,7 +1,7 @@
 print('Desafio 017 - Python - Curso em Video')
-import math
-print('Qual o valor da hipotenusa do triângulo retângulo?')
-co = int(input('Qual o valor do cateto oposto? '))
-ca = int(input('Qual o valor do cateto adjacente? '))
-sqc = pow(co, 2) + pow(ca, 2)
-print('A hipotenusa deste triângulo retângulo é {:.2f}!'.format(math.sqrt(sqc)))
+from math import hypot
+print('Vamos ver quanto mede a hipotenusa do triângulo retângulo!')
+co = float(input('Qual o valor do cateto oposto? '))
+ca = float(input('Qual o valor do cateto adjacente? '))
+hi = hypot(co, ca)
+print('A hipotenusa deste triângulo retângulo vai medir {:.2f}!'.format(hi))
